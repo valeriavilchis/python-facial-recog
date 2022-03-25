abce1a44f1ef0ebee67ee8159349424ab8f5b468
valeriavilchis <21020858@es.uveg.edu.mx>
2022-03-25 00:10:56 -0600

    second exercises

---
+++ Ejercicios/EjercicioDos.py
@@ -0,0 +1,10 @@
+#Segundo ejercicio
+from numpy import intp
+
+
+value_a = input("Valor de A - ")
+value_b = input("Valor de B - ")
+
+value_a, value_b = value_b, value_a
+
+print(f'El valor de A - {value_a} \n El valor de B - {value_b}')
